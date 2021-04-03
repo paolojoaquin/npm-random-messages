01d804dca9bcd59ec3140f19558405e28a7c1a3e
Paolo Pinto <paolojoaquinpintoperez@gmail.com>
2021-04-03 10:51:58 -0400

    First commit npm-random-messages

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const messages = [
+    "Paolo",
+    "Caroline",
+    "Susana",
+    "Ana",
+    "Nicolay",
+    "Jessica",
+    "Diego",
+    "Laura"
+];
+
+const randomMsg = () => {
+    const message = messages[Math.floor(Math.random() * messages.length)];
+    console.log(message);
+};
+
+module.exports = { randomMsg };
